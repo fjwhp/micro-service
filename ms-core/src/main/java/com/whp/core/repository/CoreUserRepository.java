c6449d07a0b3848f247ed1eb1407a3a5eaabc2a2
whp <909296452@qq.com>
2018-04-28 21:33:28 +0800

    update

--- ms-core/src/main/java/com/whp/core/repository/CoreUserRepository.java
+++ ms-core/src/main/java/com/whp/core/repository/CoreUserRepository.java
@@ -2,6 +2,7 @@ package com.whp.core.repository;
 
 import com.whp.core.pojo.CoreUser;
 import com.whp.core.repository.BaseRepository;
+
 /**
  * 
  * @描述：用户表(dao接口).
@@ -11,6 +12,5 @@ import com.whp.core.repository.BaseRepository;
  * @时间: 2018-04-28
  */
 public interface CoreUserRepository extends BaseRepository<CoreUser, Long> {
-
 
 }
