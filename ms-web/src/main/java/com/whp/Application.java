5bd90b8d8146c010576d5f8465fed1e3fb467f32
吴惠平 <1032998173@qq.com>
2018-05-04 18:19:22 +0800

    update

--- ms-web/src/main/java/com/whp/Application.java
+++ ms-web/src/main/java/com/whp/Application.java
@@ -3,7 +3,13 @@ package com.whp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.whp.core.event.EnvironmentPreparedEvent;
+import com.whp.core.event.PreparedEvent;
+import com.whp.core.event.ReadyEvent;
+import com.whp.utils.SpringContextUtil;
 
 /**
  * 
@@ -22,6 +28,16 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        // 环境已经准备完毕，上下文还没有创建[事件]
+        application.addListeners(new EnvironmentPreparedEvent());
+        // 上下文创建完成[事件]
+        application.addListeners(new PreparedEvent());
+        // 项目启动完毕[事件]
+        application.addListeners(new ReadyEvent());
+        // 启动
+        ApplicationContext context = application.run(args);
+        // 设置上下文到SpringContextUtil
+        SpringContextUtil.setApplicationContext(context);
     }
 }
