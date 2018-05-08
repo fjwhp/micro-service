b543e4d33d0c353030533ea7eaaf2e4615ada13d
whp <909296452@qq.com>
2018-05-08 20:44:55 +0800

    update

---
+++ ms-user/src/main/java/com/whp/user/pojo/PublicProperty.java
@@ -0,0 +1,23 @@
+package com.whp.user.pojo;
+
+import java.sql.Date;
+
+public class PublicProperty {
+
+	private  String createUserName;
+	private Date createDate;
+	public String getCreateUserName() {
+		return createUserName;
+	}
+	public void setCreateUserName(String createUserName) {
+		this.createUserName = createUserName;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+}
---
+++ ms-user/src/main/java/com/whp/user/pojo/Role.java
@@ -0,0 +1,19 @@
+package com.whp.user.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long roleId;
+	@Column(columnDefinition="角色名称")
+	private String roleName;
+	@Column(columnDefinition="创建人id")
+	private long createUserId;
+}
