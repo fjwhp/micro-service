c5996cd76e275f563d4835882d3ef8ecea0365fc
吴惠平 <1032998173@qq.com>
2018-05-10 17:37:06 +0800

    微服务

---
+++ ms-eureka-client/src/main/java/com/whp/EurekaClientApplication.java
@@ -0,0 +1,21 @@
+package com.whp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 
+ * @描述：TODO
+ *
+ * @作者：wuhp
+ * 
+ * @时间：2018年5月10日 下午5:01:55
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaClientApplication.class).web(true).run(args);
+    }
+}
--- ms-eureka-server/src/main/java/com/whp/eurekaserver/EurekaServerApplication.java
+++ ms-eureka-server/src/main/java/com/whp/eurekaserver/EurekaServerApplication.java
@@ -1,14 +1,22 @@
 package com.whp.eurekaserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * 
+ * @描述：服务注册中心
+ *
+ * @作者：wuhp
+ * 
+ * @时间：2018年5月10日 下午5:01:32
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		//SpringApplication.run(EurekaServerApplication.class, args);
+	    new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
 	}
 }
