404c8ce35097c6d0b5cef22ad8bc81603221f93e
吴惠平 <1032998173@qq.com>
2018-05-11 16:21:34 +0800

    update

---
+++ ms-config-server-git/src/main/java/com/whp/ConfigServiceApplication.java
@@ -0,0 +1,21 @@
+package com.whp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * 
+ * @描述：构建分布式配置中心
+ *
+ * @作者：wuhp
+ * 
+ * @时间：2018年5月11日 下午2:18:35
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServiceApplication.class).web(true).run(args);
+    }
+}
